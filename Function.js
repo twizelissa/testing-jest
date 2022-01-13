9a3ee47df96c8ff86f44762b43360732bee20493
twizelissa <twizelissa@gmail.com>
2022-01-13 21:45:47 +0200

    adding multiplication test

--- Function.js
+++ Function.js
@@ -1,4 +1,5 @@
 const functions = {
 add:(num1,num2)=>num1+num2,
+mult:(num1,num2,num3)=>num1*num2*num3
 }
 module.exports = functions;
--- Function.test.js
+++ Function.test.js
@@ -1,4 +1,7 @@
 const functions=require('./Function');
 test('Add 2 +4 to equal 6',()=>{
-    expect(functions.add(4,2)).toBe(6)
-});
+    expect(functions.add(4,2)).toBe(6)   
+}),
+test('Multiplication of 2*4*8',()=>{
+    expect(functions.mult(2,4,8)).toBe(64)
+})
